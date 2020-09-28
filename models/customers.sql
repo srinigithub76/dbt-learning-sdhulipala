0aeed3b6b1405716ece219f59de4496e99bd1369
Srini Dhulipala <srinivasarao.dhulipala@usaa.com>
2020-09-28 16:47:20 +0000

    first lab done

--- models/customers.sql
+++ models/customers.sql
@@ -8,7 +8,7 @@ with customers as (
 
 orders as (
 
-    select * from {{ ref('stage_orders') }}
+    select * from {{ ref('orders') }}
 
 ),
 
@@ -20,13 +20,13 @@ customer_orders as (
         min(order_date) as first_order_date,
         max(order_date) as most_recent_order_date,
         count(order_id) as number_of_orders,
+        sum(amount) as lifetime_value
 
     from orders
 
     group by 1
 
 ),
-
 
 final as (
 
@@ -36,7 +36,8 @@ final as (
         customers.last_name,
         customer_orders.first_order_date,
         customer_orders.most_recent_order_date,
-        coalesce(customer_orders.number_of_orders, 0) as number_of_orders
+        coalesce(customer_orders.number_of_orders, 0) as number_of_orders,
+        customer_orders.lifetime_value as lifetime_value
 
     from customers
 
